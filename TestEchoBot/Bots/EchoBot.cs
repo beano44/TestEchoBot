dba28d6eb1be3cf397e324845015350da4de9236
Sean Gordon <sean_gordon@live.co.za>
2019-11-18 14:15:10 +0200

    First Commit

--- TestEchoBot/Bots/EchoBot.cs
+++ TestEchoBot/Bots/EchoBot.cs
@@ -3,24 +3,117 @@
 //
 // Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
 
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using Microsoft.Bot.Connector;
+
+using Npgsql;
+using System.Net.Http;
+using System.Net;
 
 namespace TestEchoBot.Bots
 {
     public class EchoBot : ActivityHandler
     {
+        public static string NEWconnString = "Host=chatbot.inteleqt.co.za;Username=postgres;Password=Inteleqt$$135;Database=BlueLabelCIC;Port=5432";
+
+
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            string message = turnContext.Activity.Text;
+            string message = turnContext.Activity.Text.ToLower();
 
-            if (message.Equals("Hello"))
+
+
+            if (message.Equals("hello"))
             {
 
                 await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak("Hello How are you?"), cancellationToken);
+
+            }
+
+            else if (message.Equals("test"))
+            {
+
+                Int32 usercount = 0;
+
+                using (var conn = new NpgsqlConnection(NEWconnString))
+                {
+
+                    conn.Open();
+
+                    using (var cmd = new NpgsqlCommand("SELECT count(DISTINCT \"menu1\") FROM public.\"DecisionTreeMapping\";"
+                                                        , conn))
+                    using (var reader = cmd.ExecuteReader())
+                        while (reader.Read())
+                        {
+                            usercount = reader.GetInt32(0);
+                        }
+
+                    await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak("Demo number = " + usercount.ToString()), cancellationToken);
+
+                   await conn.CloseAsync();
+                }
+
+                
+
+                //await turnContext.SendActivityAsync(MessageFactory.SuggestedActions("string1","null")), cancellationToken);
+
+
+            }
+
+            else if (message.Equals("file"))
+            {
+
+                try
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Attachment(GetInlineAttachment()),cancellationToken);
+                    //WebClient client = new WebClient();
+                    //client.Proxy = WebRequest.DefaultWebProxy;
+                    //client.Credentials = new NetworkCredential("inteleqt-admin", "P@ssword$$01");
+                    //client.DownloadFile(@"\\\np.inteleqt.co.za\Reports\Cargo\Cargo Chatbot - Hourly.pdf", @"D:\Desktop\Blue Label Content\Final\Documents\Chatbot.PDF");
+
+                }
+                catch (Exception e)
+                {
+
+                    string exception = e.ToString();
+
+                    await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak(exception), cancellationToken);
+
+                }
+
+
+
+
+            }
+
+            else if (message.Equals("ask"))
+            {
+                var reply = MessageFactory.Text("What is your favorite color?");
+                reply.SuggestedActions = new SuggestedActions()
+                {
+                    Actions = new List<CardAction>()
+                {
+                    new CardAction() { Title = "Red", Type = ActionTypes.ImBack, Value = "#test" },
+                    new CardAction() { Title = "Yellow", Type = ActionTypes.ImBack, Value = "Yellow" },
+                    new CardAction() { Title = "Blue", Type = ActionTypes.ImBack, Value = "Blue" },
+                },
+                };
+                await turnContext.SendActivityAsync(reply, cancellationToken);
+            }
+
+            else if (message.Equals("#test"))
+            {
+
+                await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak("Received the test"), cancellationToken);
+
 
             }
 
@@ -30,10 +123,7 @@ namespace TestEchoBot.Bots
                 await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"You said:\"{turnContext.Activity.Text}\""), cancellationToken);
 
             }
-
-            //await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"Echo: {turnContext.Activity.Text}"), cancellationToken);
-
-
+                       
         }
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
@@ -45,6 +135,20 @@ namespace TestEchoBot.Bots
                     await turnContext.SendActivityAsync(CreateActivityWithTextAndSpeak($"Hello and welcome!"), cancellationToken);
                 }
             }
+        }
+
+        private static Attachment GetInlineAttachment()
+        {
+            
+            var imagePath = @"\\np.inteleqt.co.za\Reports\Cargo\Cargo Chatbot - Hourly.pdf";
+            var imageData = Convert.ToBase64String(File.ReadAllBytes(imagePath));
+
+            return new Attachment
+            {
+                Name = @"AccountsQueries.pdf",
+                ContentType = "Application/pdf",
+                ContentUrl = $"data:image/png;base64,{imageData}",
+            };
         }
 
         private IActivity CreateActivityWithTextAndSpeak(string message)
